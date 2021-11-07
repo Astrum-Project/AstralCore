7f03a7ab5a2265f853013f57619feef436fec37b
xKiraiChan <xKiraiChan@gmail.com>
2021-11-07 01:16:29 -0400

    add hooks, convar, events

--- AstralCore.cs
+++ AstralCore.cs
@@ -1,11 +1,19 @@
 ﻿using MelonLoader;
 using System;
 
-[assembly: MelonInfo(typeof(Astrum.AstralCore.Loader), "AstralCore", "0.1.0", downloadLink: "github.com/Astrum-Project/AstralCore")]
+[assembly: MelonInfo(typeof(Astrum.AstralCore.Loader), "AstralCore", "0.2.0", downloadLink: "github.com/Astrum-Project/AstralCore")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 
 namespace Astrum.AstralCore
 {
-    public class Loader : MelonMod { }
+    public class Loader : MelonMod 
+    {
+        public override void OnApplicationStart()
+        {
+            Hooks.Hooks.Initialize(HarmonyInstance);
+        }
+
+        public override void OnUpdate() => Events.OnUpdate();
+    }
 }
--- Managers/CommandManager.cs
+++ Managers/CommandManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Astrum.AstralCore.Managers
 {
@@ -15,8 +16,16 @@ namespace Astrum.AstralCore.Managers
 
             new Command
             {
+                onExecute = new Func<string[], string>(_ => {
+                    Environment.Exit(0);
+                    return "";
+                })
+            }.Register("exit", "quit", "abort");
+
+            new Command
+            {
                 onExecute = new Func<string[], string>(args => "Reserved for future use")
-            }.Register("get", "set", "list", "help");
+            }.Register("list", "help");
         }
 
         public static Dictionary<string, Command> commands = new Dictionary<string, Command>(StringComparer.OrdinalIgnoreCase);
@@ -40,6 +49,51 @@ namespace Astrum.AstralCore.Managers
             public void Register(string name) => commands.Add(name, this);
             public void Register(params string[] aliases) => aliases.ToList().ForEach(f => Register(f));
             public void Unregister() => commands.Where(f => f.Value == this).Select(f => f.Key).ToList().ForEach(f => CommandManager.Unregister(f));
+        }
+
+        public class ConVar
+        {
+            public Action<object> onChange;
+            private Command command;
+
+            public ConVar(Action<object> onChange, Func<string, object> tryParse) 
+            {
+                this.onChange = onChange;
+
+                command = new Command
+                {
+                    onExecute = new Func<string[], string>(args =>
+                    {
+                        object res = tryParse(string.Join(" ", args));
+                        if (res is null) return "Invalid input";
+                        
+                        onChange(res);
+                        return "";
+                    })
+                };
+            }
+
+            public void Register(string name) => command.Register(name);
+            public void Register(params string[] aliases) => command.Register(aliases);
+            public void Unregister() => command.Unregister();
+
+            public static Func<string, object> TryParseBool = new Func<string, object>(s =>
+            {
+                if (bool.TryParse(s, out bool result)) return result;
+                return null;
+            });
+            public static Func<string, object> TryParseInt = new Func<string, object>(s =>
+            {
+                if (int.TryParse(s, out int result)) return result;
+                return null;
+            });
+            public static Func<string, object> TryParseFloat = new Func<string, object>(s =>
+            {
+                if (float.TryParse(s, out float result)) return result;
+                return null;
+            });
+            public static Func<string, object> TryParseString = new Func<string, object>(s => s);
+
         }
     }
 }
